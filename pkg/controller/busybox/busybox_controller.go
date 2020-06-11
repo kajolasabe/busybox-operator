185baac4180dad55ac8b03c1252ad7063c0084c1
kajolasabe <66050643+kajolasabe@users.noreply.github.com>
2020-06-11 12:45:28 +0530

    added appsv1

--- pkg/controller/busybox/busybox_controller.go
+++ pkg/controller/busybox/busybox_controller.go
@@ -3,6 +3,7 @@ package busybox
 import (
 	"context"
 
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	//metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
